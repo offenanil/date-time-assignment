ea674d4c00ffc7d6ed5046bc04e7a64ee57210c1
Anil Acharya <86336649+offenanil@users.noreply.github.com>
2021-08-11 18:07:55 +1000

    Update date_time_assignment.py

--- date_time_assignment.py
+++ date_time_assignment.py
@@ -4,6 +4,12 @@ job_post = datetime.date.today()
 job_exp = datetime.timedelta(days = 12)
 job_valid = job_post + job_exp
 
+print(job_post)
+print(job_post.day)
+print(job_post.month)
+print(job_post.year)
+print(job_post.ctime())
+
 print(f"job expires on {job_valid}")
 # print(f"job expires on {job_post + job_exp}")
 
